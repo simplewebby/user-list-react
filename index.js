799df69e42b07230c1a6cb123ad30d5739502c4d
Tsagan Garyaeva <simplewebby@outlook.co>
2019-01-30 12:53:12 -0500

    This is a new commit for whatd

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const items = require('./routes/api/items')
 const path = require('path')
 
 const app= express();
 
-const port = process.env.PORT || 5000;
+
 
 // Body Parser Middleware
 app.use(bodyParser.json());
@@ -22,15 +21,17 @@ mongoose
     .catch(err => console.log(err));
 
 // use routes
-app.use('/api/items/', items);
+app.use('/api/items', items);
 
 // Serve static assets
 if(process.env.NODE_ENV === "production"){
 app.use(express.static('client/build'));
+
+
 app.get('*', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
 });
 }
-
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
