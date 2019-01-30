27b62d5dd87a6cb1844bd4911c61fbe27af0ac12
Tsagan Garyaeva <simplewebby@outlook.co>
2019-01-30 11:51:22 -0500

    better

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ mongoose
 app.use('/api/items/', items);
 
 // Serve static assets
-//if(process.env.NODE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
 app.use(express.static('client/build'));
 app.get('*', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
 });
-//}
+}
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
